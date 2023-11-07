e707aafa8d8352ba6988b00d2cedaf1cbdc6727d
macucaires27 <macuco@fp.insjoaquimmir.cat>
2023-11-07 16:35:21 +0100

    Mail controller

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-use Illuminate\Support\Facades\Log;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,8 +13,42 @@ use Illuminate\Support\Facades\Log;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
-
+ 
 Route::get('/', function () {
-    Log::info('Loading welcome page');
     return view('welcome');
 });
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+#------Ruta Request
+
+use Illuminate\Http\Request;
+// ...
+Route::get('/dashboard', function (Request $request) {
+    $request->session()->flash('info', 'TEST flash messages');
+    return view('dashboard');
+ })->middleware(['auth','verified'])->name('dashboard');;
+
+
+
+
+#-------------Ruta MailController
+use App\Http\Controllers\MailController;
+// ...
+Route::get('mail/test', [MailController::class, 'test']);
+
+// or
+// Route::get('mail/test', 'App\Http\Controllers\MailController@test');
+
+
+
+
+require __DIR__.'/auth.php';
