e6ef900a61b348987397be201e21117e1adfb645
PolitoLimon <pfenori@fp.insjoaquimmir.cat>
2024-02-15 19:49:26 +0100

    SEGUIR EN CASA

--- laravel/resources/views/dashboard.blade.php
+++ laravel/resources/views/dashboard.blade.php
@@ -7,11 +7,11 @@
 @section('box-content')
     <p class="mb-4">{{ __("You're logged in!") }}</p>
     <h2 class="mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-white">{{ __('Resources') }}</h2>
-    @can('viewAny', App\Models\File::class)
+
     <x-primary-button href="{{ url('/files') }}">
         🗄️ {{ __('Files') }}
     </x-primary-button>
-    @endcan
+
     @can('viewAny', App\Models\Post::class)
     <x-primary-button href="{{ url('/posts') }}">
         📑 {{ __('Posts') }}        
